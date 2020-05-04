5223716ac00008b701b4ac51bafd5283957d3f55
Arjun <arjn.nair@gmail.com>
2020-05-04 03:33:30 -0400

    unit test

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 )
 
 type Simple struct {
-	Name string
+	Name        string
 	Description string
-	Url string
+	Url         string
 }
 
+func SimpleFactory(host string) Simple {
+	return Simple{"Simple", "Test", host}
+}
 func handler(w http.ResponseWriter, r *http.Request) {
-	simple := Simple{"Hello", "World",r.Host}
+	simple := SimpleFactory(r.Host)
 
 	jsonOutput, _ := json.Marshal(simple)
 
@@ -24,5 +27,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Server started")
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
